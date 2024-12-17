2b2ea5fd974b72f53e95bdd7b6f7fe644a6a539f
Sobirjon <151761986+abdurasulovsobirjon1120@users.noreply.github.com>
2024-12-17 08:55:35 +0500

    Update admin.py

--- balance/student_balance/admin.py
+++ balance/student_balance/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Student
 
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'start_date', 'balance', 'status')
+    ordering = ('-id',)
+    search_fields = ('first_name', 'last_name')
+
+
+admin.site.register(Student, StudentAdmin)
